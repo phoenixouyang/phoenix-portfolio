e832206cb6f3a401526f900c13c72ee11aa6d941
unknown <ouyangphoenix@gmail.com>
2025-12-13 17:19:38 -0500

    add background and profile picture to home section

--- obeserver.js
+++ obeserver.js
@@ -1,3 +1,20 @@
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((elements) => {
+    elements.forEach((e) => {
+        if (e.isIntersecting) {
+            e.target.classList.add("in-view");
+            e.target.classList.remove("not-in-view");
+        } else {
+            e.target.classList.remove("in-view");
+            e.target.classList.add("not-in-view");
+        }
+    });
+}, {
+    rootMargin: "0px",
+    threshold: [0, 0.1, 1]
+});
 
-}, {});
+const tags = document.querySelectorAll("h2, h3");
+
+tags.forEach((tag) => {
+    observer.observe(tag);
+});
